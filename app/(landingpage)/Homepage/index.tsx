2d8f27f7ff714ce352b965acf405527269265b58
45szn <israelxxn@example.com>
2025-06-06 07:33:14 +0100

    added a new section

--- app/(landingpage)/Homepage/index.tsx
+++ app/(landingpage)/Homepage/index.tsx
@@ -106,6 +106,32 @@ const Homepage = () => {
         <section>
           <Plans />
         </section>
+
+        <section>
+          <div className="bg-[#16161635] py-20">
+            <div className="container mx-auto px-4 xl:px-0">
+              <h1 className="text-4xl font-bold text-center lg:text-5xl">You want to start investing with TRUSTFX?</h1>
+
+              <div className="flex flex-col gap-8 my-14 lg:flex-row">
+                <div className="bg-[#161616] text-white text-center rounded-xl flex flex-col gap-3 p-10 shadow-xl">
+                  <div className="bg-black mx-auto rounded-full w-[3.25rem] p-3"><span className="font-bold text-xl">1</span></div>
+                  <p className="text-[1.4rem] font-bold">Create an account</p>
+                  <p className="text-sm font-semibold">Create an account providing your valid information</p>
+                </div>
+                <div className="bg-[#161616] text-white text-center rounded-xl flex flex-col gap-3 p-10 shadow-xl">
+                  <div className="bg-black mx-auto rounded-full w-[3.25rem] p-3"><span className="font-bold text-xl">2</span></div>
+                  <p className="text-[1.4rem] font-bold">Choose plan</p>
+                  <p className="text-sm font-semibold">Make investment to get profit from our system</p>
+                </div>
+                <div className="bg-[#161616] text-white text-center rounded-xl flex flex-col gap-3 p-10 shadow-xl">
+                  <div className="bg-black mx-auto rounded-full w-[3.25rem] p-3"><span className="font-bold text-xl">1</span></div>
+                  <p className="text-[1.4rem] font-bold">Get profit</p>
+                  <p className="text-sm font-semibold">Withdraw your profit wyou earned from your investment</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
