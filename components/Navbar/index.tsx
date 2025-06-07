35aa7ffa39e27af32da605ffab2a0e100c17da34
45szn <israelxxn@example.com>
2025-06-07 17:43:20 +0100

    added langingpage layout

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -13,10 +13,10 @@ export default function Navbar() {
   };
 
   const navItems = [
-    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
     { name: "Plans", href: "/#" },
+    { name: "Testimonials", href: "/#" },
     { name: "Contact", href: "/#" },
-    { name: "Testimonials", href: "/testimonials" },
     { name: "Register", href: "/register" },
   ];
 
