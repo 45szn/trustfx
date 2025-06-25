b05c51d998382c14ae5ac63fd057920780bd327c
45szn <israelxxn@example.com>
2025-06-25 14:14:40 +0100

    bug fix

--- app/(landingpage)/blogs/[slug]/page.tsx
+++ app/(landingpage)/blogs/[slug]/page.tsx
@@ -1,12 +1,11 @@
-import { Metadata } from "next"
-import BlogPost from "../components/BlogPost"
-import RelatedPosts from "../components/RelatedPosts"
-import BlogNewsletter from "../components/BlogNewsletter"
+import BlogPost from "../components/BlogPost";
+import RelatedPosts from "../components/RelatedPosts";
+import BlogNewsletter from "../components/BlogNewsletter";
 
 interface PageProps {
   params: {
-    slug: string
-  }
+    slug: string;
+  };
 }
 
 export default function BlogPostPage({ params }: PageProps) {
@@ -16,23 +15,7 @@ export default function BlogPostPage({ params }: PageProps) {
       <RelatedPosts />
       <BlogNewsletter />
     </>
-  )
-}
-
-// optional for SEO
-export function generateMetadata({ params }: PageProps): Metadata {
-  return {
-    title: `${params.slug.replace(/-/g, " ")} | Blog - TrustFx`,
-  }
-}
-
-export async function generateStaticParams() {
-  return [
-    { slug: "market-volatility-2024-strategies" },
-    { slug: "cryptocurrency-traditional-portfolios" },
-    { slug: "risk-management-101" },
-    { slug: "the-future-of-ai-in-investment-management" },
-  ]
+  );
 }
 
 
