9236acfbbe3da388be88362ca9eb38088a753670
45szn <israelxxn@example.com>
2025-06-25 14:10:09 +0100

    bug fix

--- app/(landingpage)/blogs/[slug]/page.tsx
+++ app/(landingpage)/blogs/[slug]/page.tsx
@@ -1,22 +1,40 @@
+import { Metadata } from "next"
 import BlogPost from "../components/BlogPost"
 import RelatedPosts from "../components/RelatedPosts"
 import BlogNewsletter from "../components/BlogNewsletter"
 
-type Props = {
-  params: { slug: string }
+interface PageProps {
+  params: {
+    slug: string
+  }
 }
 
-export default function BlogPostPage({ params }: Props) {
-  const { slug } = params
-
+export default function BlogPostPage({ params }: PageProps) {
   return (
     <>
-      <BlogPost slug={slug} />
+      <BlogPost slug={params.slug} />
       <RelatedPosts />
       <BlogNewsletter />
     </>
   )
 }
+
+// optional for SEO
+export function generateMetadata({ params }: PageProps): Metadata {
+  return {
+    title: `${params.slug.replace(/-/g, " ")} | Blog - TrustFx`,
+  }
+}
+
+export async function generateStaticParams() {
+  return [
+    { slug: "market-volatility-2024-strategies" },
+    { slug: "cryptocurrency-traditional-portfolios" },
+    { slug: "risk-management-101" },
+    { slug: "the-future-of-ai-in-investment-management" },
+  ]
+}
+
 
 
 
