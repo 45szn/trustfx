f44d11f9b54ac7c5045b590de5f7f59643d2e28e
45szn <israelxxn@example.com>
2025-07-01 06:46:17 +0100

    fix 4

--- app/(landingpage)/blogs/[slug]/page.tsx
+++ app/(landingpage)/blogs/[slug]/page.tsx
@@ -1,36 +1,10 @@
-// app/(landingpage)/blogs/[slug]/page.tsx
-import BlogPost from "../components/BlogPost";
-import RelatedPosts from "../components/RelatedPosts";
-import BlogNewsletter from "../components/BlogNewsletter";
-
-export const dynamic = "force-static"; // Optional: if you use static generation
-
-const BlogPostPage = ({ params }: { params: { slug: string } }) => {
-  return (
-    <>
-      <BlogPost slug={params.slug} />
-      <RelatedPosts />
-      <BlogNewsletter />
-    </>
-  );
-};
-
-export default BlogPostPage;
-
-
-
-
-
 // // app/(landingpage)/blogs/[slug]/page.tsx
 // import BlogPost from "../components/BlogPost";
 // import RelatedPosts from "../components/RelatedPosts";
 // import BlogNewsletter from "../components/BlogNewsletter";
 
-// interface PageProps {
-//   params: { slug: string };
-// }
-
-// export default async function BlogPostPage({ params }: PageProps) {
+// // ✅ DO NOT type 'params' manually at the function level
+// export default function BlogPostPage({ params }: { params: { slug: string } }) {
 //   return (
 //     <>
 //       <BlogPost slug={params.slug} />
@@ -39,3 +13,26 @@ export default BlogPostPage;
 //     </>
 //   );
 // }
+
+
+
+
+
+// app/(landingpage)/blogs/[slug]/page.tsx
+import BlogPost from "../components/BlogPost";
+import RelatedPosts from "../components/RelatedPosts";
+import BlogNewsletter from "../components/BlogNewsletter";
+
+interface PageProps {
+  params: { slug: string };
+}
+
+export default async function BlogPostPage({ params }: PageProps) {
+  return (
+    <>
+      {/* <BlogPost slug={params.slug} /> */}
+      <RelatedPosts />
+      <BlogNewsletter />
+    </>
+  );
+}
