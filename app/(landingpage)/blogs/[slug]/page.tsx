732b381e13ad1f3ea2f5d6e92ec97f04c7145c13
45szn <israelxxn@example.com>
2025-07-10 15:36:26 +0100

    fix 7

--- app/(landingpage)/blogs/[slug]/page.tsx
+++ app/(landingpage)/blogs/[slug]/page.tsx
@@ -1,15 +1,16 @@
 import BlogPost from "../components/BlogPost";
 import RelatedPosts from "../components/RelatedPosts";
 import BlogNewsletter from "../components/BlogNewsletter";
+// import { use } from "react";
 
 // interface PageProps {
 //   params: { slug: string };
 // }
 
-export default function BlogPostPage({ params }: { params: { slug: string } }) {
+export default async function BlogPostPage({ params }: {params: Promise<{ slug: string }>}) {
   return (
     <>
-      <BlogPost slug={params.slug} />
+      <BlogPost slug={(await params).slug} />
       <RelatedPosts />
       <BlogNewsletter />
     </>
